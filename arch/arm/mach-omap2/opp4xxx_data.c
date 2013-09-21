35e0c0455cb7ae4beb50375c5873b328a76a1087
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2013-09-21 11:03:36 +0700

    mach-omap2: rewriting overclock code

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -138,6 +138,9 @@ struct omap_volt_data omap443x_vdd_iva_volt_data[] = {
 
 #define OMAP4430_VDD_CORE_OPP50_UV		1025000
 #define OMAP4430_VDD_CORE_OPP100_UV		1200000
+#ifdef CONFIG_OMAP4430_GPU_OVERCLOCK
+#define OMAP4430_VDD_CORE_OPP100_OV_UV		1250000
+#endif
 
 struct omap_volt_data omap443x_vdd_core_volt_data[] = {
 	VOLT_DATA_DEFINE(OMAP4430_VDD_CORE_OPP50_UV, 0,
@@ -146,6 +149,11 @@ struct omap_volt_data omap443x_vdd_core_volt_data[] = {
 	VOLT_DATA_DEFINE(OMAP4430_VDD_CORE_OPP100_UV, 0,
 			OMAP44XX_CONTROL_FUSE_CORE_OPP100,
 			0xf9, 0x16, OMAP_ABB_NONE),
+#ifdef CONFIG_OMAP4430_GPU_OVERCLOCK
+	VOLT_DATA_DEFINE(OMAP4430_VDD_CORE_OPP100_OV_UV, 0,
+			OMAP44XX_CONTROL_FUSE_CORE_OPP100OV,
+			0xf9, 0x16, OMAP_ABB_NONE),
+#endif
 	VOLT_DATA_DEFINE(0, 0, 0, 0, 0, 0),
 };
 
@@ -237,6 +245,11 @@ static struct omap_opp_def __initdata omap443x_opp_def_list[] = {
 	/* SGX OPP2 - OPP100 */
 	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", true,
 			307200000, OMAP4430_VDD_CORE_OPP100_UV),
+#ifdef CONFIG_OMAP4430_GPU_OVERCLOCK
+	/* SGX OPP3 - OPPOV */
+	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", false,
+			384000000, OMAP4430_VDD_CORE_OPP100_OV_UV),
+#endif
 	/* FDIF OPP1 - OPP25 */
 	OPP_INITIALIZER("fdif", "fdif_fck", "core", true,
 			32000000, OMAP4430_VDD_CORE_OPP50_UV),
@@ -987,6 +1000,14 @@ int __init omap4_opp_init(void)
 	if (omap4_has_mpu_1_5ghz() && trimmed)
 		omap4_opp_enable("mpu", 1500000000);
 	*/
+
+#ifdef CONFIG_OMAP4430_CPU_OVERCLOCK
+	omap4_opp_enable("mpu", 1200000000);
+#endif
+
+#ifdef CONFIG_OMAP4430_GPU_OVERCLOCK
+	omap4_opp_enable("gpu", 384000000);
+#endif
 out:
 	return r;
 }
